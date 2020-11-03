d94a8a58e0f5fa4962124abc04ae381de821b139
saurabh rawat <saurabh.rawat@fluper.in>
2020-11-03 15:24:57 +0530

    multer

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,6 @@
 const { productModel } = require("../models/product");
 const { cateogryModel } = require("../models/Category");
-const {subcategoryModel} = require ('../models/subcategory');
+const { subcategoryModel } = require("../models/subcategory");
 const joi = require("joi");
 const { Error } = require("mongoose");
 const { generateToken, sendotp } = require("../modules/commonFunctions");
@@ -12,19 +12,36 @@ const mongoose = require("../modules/db_connection");
 
 exports.addProduct = async function (req, res) {
   try {
-    let {name,subcategoryId,categoryId,price} = req.body
+    let data = req.body;
+    let file = req.files;
+
+    console.log(data,file,"lk");
+    let name = req.body.name;
     // console.log("hit",data.name)
     let isProductExist = await productModel.findOne({
-      name:name,
+      name: name,
     });
     if (isProductExist) {
       throw new Error(`${name}  product is already defiend.`);
     }
-    let a = new productModel(req.body);
+    if(file){
+      console.log(file[0].filename,"filename")
+      data.image = `/productimage/${file[0].filename}`
+    }
+
+    let a = new productModel({
+      name: data.name,
+      price:data.price,
+      subcategoryId:data.subcategoryId,
+      categoryId:data.categoryId,
+      image:data.image,
+      description:data.description,
+      brand:data.brand,
+    });
+    console.log("hit",a);
     let saveProduct = await a.save();
-    console.log("hit", saveProduct);
     if (!saveProduct) {
-      throw new Error("unable to add the Product ");
+      throw new Error("unable to add the Product");
     }
     res.json({ message: saveProduct });
   } catch (err) {
@@ -32,15 +49,15 @@ exports.addProduct = async function (req, res) {
   }
 };
 
-
 //---------------------------------------------------------update subcategory with id -------------------------------------------------------------------------------------
-
 
 exports.updateProduct = async (req, res) => {
   try {
-    let updateData = await productModel.findOneAndUpdate({
-      _id: req.body.id || req.headers.id || req.params.id},
-      { $set :req.body  },
+    let updateData = await productModel.findOneAndUpdate(
+      {
+        _id: req.body.id || req.headers.id || req.params.id,
+      },
+      { $set: req.body },
       { new: true }
     );
     if (updateData) {
@@ -55,30 +72,28 @@ exports.updateProduct = async (req, res) => {
 
 //--------------------------------------------------------------delet wiith id-------------------------------------------------------
 
-exports.deleteProduct = async (req,res) => {
-    try{
-      let delProduct = await subcategoryModel.deleteOne({_id:req.body.id});
-      if(!delProduct){
-        throw new Error("Cant delete product right now. ");
-      }
-      return res.json({deletedData:delProduct});
-    }catch (error) {
-      res.status(401).json({ message: error });
+exports.deleteProduct = async (req, res) => {
+  try {
+    let delProduct = await subcategoryModel.deleteOne({ _id: req.body.id });
+    if (!delProduct) {
+      throw new Error("Cant delete product right now. ");
     }
-    
-    }
+    return res.json({ deletedData: delProduct });
+  } catch (error) {
+    res.status(401).json({ message: error });
+  }
+};
 
 //=-----------------------------------------------------get all products--------------------------------------------------------------//
-exports.getAllProducts = async (req,res) =>{
-  try{
+exports.getAllProducts = async (req, res) => {
+  try {
     let getProducts = await productModel.find({});
-    if (getProducts){
+    if (getProducts) {
       res.status(200).json({ "All products are  ": getProducts });
-  }
-}catch (error) {
+    }
+  } catch (error) {
     res.status(401).json({ message: error.message });
   }
-}
-
+};
 
 //-------------------------------------------
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -6,46 +6,52 @@ const commonFunctions = require("../modules/commonFunctions")
 const categoryController = require("../controllers/categoryController");
 const subcategoryController = require("../controllers/subcategoryController");
 const productController = require("../controllers/productController")
+const multer = require("multer");
+var path = require("path");
+const md5 = require("md5");
+const { func } = require("joi");
+
+var storage = multer.diskStorage({
+    destination:function (req,file,cb) {
+        cb(null,"./upload/productImage")
+    },
+    filename : function(req,file,cb) {
+        cb(null,Date.now() + path.extname(file.originalname));
+    }
+});
+
+var upload = multer({storage:storage});
 
 
 
-//---------------------------------------------ROUTES for USERS------------------------------------------------------------------
+//--------------------------------------------------ROUTES for USERS------------------------------------------------------------------
 
 router.delete("/deleteuser",auth.requireToken,commonFunctions.verifyAdmin,adminController.deleteUser);  //by user and admin both
 router.get("/userdetails",auth.requireToken,adminController.getUserById); // user 
 router.get("/getAllUser",auth.requireToken,commonFunctions.verifyAdmin,adminController.getAllUser);// only by admin
 router.get("/getuser",auth.requireToken,commonFunctions.verifyAdmin,adminController.getUserById); // only by admin
-router.put("/blockandunblockuser",auth.requireToken,commonFunctions.verifyAdmin,adminController.blockAndUnblockUser); // only by admin
+router.put("/blockandunblockuser",adminController.blockAndUnblockUser); // only by admin
 
 
-//--------------------------------------------------------------routes OF PRODUCT category -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------Routes OF PRODUCT Category -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-router.post("product/addCategory",auth.requireToken,commonFunctions.verifyAdmin,categoryController.addCategory); //access only by admin
-router.get("product/getAllCategory",auth.requireToken,commonFunctions.verifyAdmin,categoryController.getAllCategory); //access only by admin
-router.put("product/deleteCategory",auth.requireToken,commonFunctions.verifyAdmin,categoryController.deleteCategory) //access only by admin
-router.put("product/updateCategory",auth.requireToken,commonFunctions.verifyAdmin,categoryController.updateCategory) //access only by admin
+router.post("/product/addCategory",auth.requireToken,commonFunctions.verifyAdmin,categoryController.addCategory); //access only by admin
+router.get("/product/getAllCategory",auth.requireToken,commonFunctions.verifyAdmin,categoryController.getAllCategory); //access only by admin
+router.put("/product/deleteCategory",auth.requireToken,commonFunctions.verifyAdmin,categoryController.deleteCategory) //access only by admin
+router.put("/product/updateCategory",auth.requireToken,commonFunctions.verifyAdmin,categoryController.updateCategory) //access only by admin
 
 //-------------------------------------------------------------routes of PRODUCT Subcategory------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-router.post("product/addsubCategory",auth.requireToken,commonFunctions.verifyAdmin,subcategoryController.addsubCategory);  //access only by admin
-router.put("product/updatesubCategory",auth.requireToken,commonFunctions.verifyAdmin,subcategoryController.updateSubCategory)  //access only by admin
-router.put("product/deletesubCategory",auth.requireToken,commonFunctions.verifyAdmin,subcategoryController.deletesubCategory);  //access only by admin
+router.post("/product/addsubCategory",auth.requireToken,commonFunctions.verifyAdmin,subcategoryController.addsubCategory);  //access only by admin
+router.put("/product/updatesubCategory",auth.requireToken,commonFunctions.verifyAdmin,subcategoryController.updateSubCategory)  //access only by admin
+router.put("/product/deletesubCategory",auth.requireToken,commonFunctions.verifyAdmin,subcategoryController.deletesubCategory);  //access only by admin
 
 //-------------------------------------------------------------ROUTES OF PRODUCTS PRODCTS------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-router.post("product/addProduct",auth.requireToken,commonFunctions.verifyAdmin,productController.addProduct);  //access only by admin
-router.put("product/updateProduct",auth.requireToken,commonFunctions.verifyAdmin,productController.updateProduct);  //access only by admin
-router.put("product/deleteProduct",auth.requireToken,commonFunctions.verifyAdmin,productController.deleteProduct); //access only by admin
-router.get("product/getallProducts",auth.requireToken,commonFunctions.verifyAdmin,productController.getAllProducts); //access only by admin
-
-
-
-
-
-
-
-
-
+router.post("/addProduct",upload.any(),productController.addProduct);  //access only by admin
+router.put("/product/updateProduct",auth.requireToken,commonFunctions.verifyAdmin,productController.updateProduct);  //access only by admin
+router.put("/product/deleteProduct",auth.requireToken,commonFunctions.verifyAdmin,productController.deleteProduct); //access only by admin
+router.get("/product/getallProducts",auth.requireToken,commonFunctions.verifyAdmin,productController.getAllProducts); //access only by admin
 
 
 module.exports = router;
